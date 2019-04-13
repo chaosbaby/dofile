c5353df7a72753559b57ad43a30085147c546de3
chaos <50092526@qq.com>
2019-04-13 10:32:30 +0800

    fix the python3 config dir

--- vim/setup_vim8.sh
+++ vim/setup_vim8.sh
@@ -5,7 +5,7 @@ sudo yum install -y ruby ruby-devel lua lua-devel luajit luajit-devel ctags git 
 sudo ln -s /usr/bin/xsubpp /usr/share/perl5/ExtUtils/xsubpp
 
 sudo yum install ncurses ncurses-devel -y
-#sudo yum install ncurses-devel.x86_64  -y
+sudo yum install ncurses-devel.x86_64  -y
 mkdir ~/tool
 cd ~/tool
 if [ -d "/root/tool/vim-master" ];then
@@ -17,17 +17,16 @@ echo "文件不存在"
 fi
 cd vim-master
 cd src/
-# ./configure --with-features=huge -enable-pythoninterp --with-python-config-dir=/usr/lib64/python2.7/config
 
-./configure --with-features=huge \ --enable-multibyte \
---enable-rubyinterp=yes \
---enable-python3interp=yes \
---with-python-config-dir=/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu \
---enable-perlinterp=yes \
---enable-luainterp=yes \
---enable-cscope \
---prefix=/usr/local
-
+./configure --with-features=huge \
+        --enable-multibyte \
+        --enable-rubyinterp=yes \
+        --enable-python3interp=yes \
+        --with-python-config-dir=/usr/local/python3/lib/python3.7/config-3.7m-x86_64-linux-gnu \
+        --enable-perlinterp=yes \
+        --enable-luainterp=yes \
+        --enable-cscope \
+        --prefix=/usr/local
 sudo make
 sudo make install
 export PATH=/usr/local/bin:$PATH
