1c26ac01277ee872051ecd1ec82f471dc2a03cc1
chaos <50092526@qq.com>
2019-04-13 09:39:19 +0800

    remove variable dofilesPath to make auto installling no hult

--- vim/setupVim8Plugins.sh
+++ vim/setupVim8Plugins.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm /root/.vimrc -f
-cp ${dofilesPath}/vim/vim8.vimrc /root/.vimrc -f
+cp vim8.vimrc /root/.vimrc -f
 vim "+PluginInstall" "+x" "+x"
 cd ~/.vim/bundle/youcompleteme
 ./install.py
