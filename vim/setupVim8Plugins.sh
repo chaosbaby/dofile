dbadcbc551417b86a60f03fef24d19daf29a2773
root <root@localhost.localdomain>
2019-04-13 09:26:43 +0800

    use force copy to ensure no hult in auto installing

--- vim/setupVim8Plugins.sh
+++ vim/setupVim8Plugins.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-rm /root/.vimrc
-ln ${dofilesPath}/vim/vim8.vimrc /root/.vimrc -s
+rm /root/.vimrc -f
+cp ${dofilesPath}/vim/vim8.vimrc /root/.vimrc -f
 vim "+PluginInstall" "+x" "+x"
 cd ~/.vim/bundle/youcompleteme
 ./install.py
