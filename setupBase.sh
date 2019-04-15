802a246253b20c820ccb794344c6613013280c6d
chaos <50092526@qq.com>
2019-04-16 01:58:20 +0800

    set path as variable for easy use

--- setupBase.sh
+++ setupBase.sh
@@ -1,3 +1,6 @@
+# base setting
+
+dofilesPath=`pwd`
 # for some more recent plugin
 sudo yum upgrade -y
 sudo yum update -y
@@ -11,25 +14,25 @@ git config --global user.name "chaos"
 git config --global credential.helper store
 
 # hub install
-cd ~/tool/dofiles/hub
-sh ~/tool/dofiles/hub/setup.sh
+cd ${doflesPath}/hub
+sh ${doflesPath}/hub/setup.sh
 
 # python3
-cd ~/tool/dofiles/python3
-sh ~/tool/dofiles/python3/setup_3.7.3.sh
+cd ${doflesPath}/python3
+sh ${doflesPath}/python3/setup_3.7.3.sh
 
 # vim8
-cd ~/tool/dofiles/vim
-sh ~/tool/dofiles/vim/setup_vim8.sh
+cd ${doflesPath}/vim
+sh ${doflesPath}/vim/setup_vim8.sh
 
 #vim8 plugins
-cd ~/tool/dofiles/vim
-sh ~/tool/dofiles/vim/setupVim8Plugins.sh
+cd ${doflesPath}/vim
+sh ${doflesPath}/vim/setupVim8Plugins.sh
 
 ## dofiles: tmux
-cd ~/tool/dofiles/tmux
-sh ~/tool/dofiles/tmux/setup.sh
+cd ${doflesPath}/tmux
+sh ${doflesPath}/tmux/setup.sh
 
 ## dofiles: zsh
-cd ~/tool/dofiles/zsh
-sh ~/tool/dofiles/zsh/setup.sh
+cd ${doflesPath}/zsh
+sh ${doflesPath}/zsh/setup.sh
