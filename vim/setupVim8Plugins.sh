12ac46ec5771890883b2b3d6a1151fcb57b4178e
chaosbaby <50092526@qq.com>
2019-04-15 22:53:11 +0800

    add centOS7 aliyun vim auto install sh

---
+++ vim/setupVim8Plugins.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm /root/.vimrc
+ln /root/tool/dofiles/vim/vim8.vimrc /root/.vimrc -s
+vim "+PluginInstall" "+x" "+x"
+cd ~/.vim/bundle/youcompleteme
+./install.py
