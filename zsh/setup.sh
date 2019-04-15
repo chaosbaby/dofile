1d157575827b3fa7810fa4585b4e4ee83bc612d6
chaosbaby <50092526@qq.com>
2019-04-15 22:53:58 +0800

    add zsh auto install sh

---
+++ zsh/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+yum install zsh -y
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    echo "Install oh-my-zsh ..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+cp -v alias.zsh ~/.alias
+cp -v zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+source ~/.zshrc
