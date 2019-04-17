19c0555021094d10d963158cd98eb488bf0a7d02
chaos <50092526@qq.com>
2019-04-17 10:53:04 +0800

    add install python3 with sll surport to ensure that pip work well

--- python3/setup_3.7.3.sh
+++ python3/setup_3.7.3.sh
@@ -2,20 +2,21 @@ yum install zlib zlib-devel -y
 yum install libffi-devel -y
 yum install unzip wget -y
 yum install ctags cmake gcc-c++ python-devel -y
+yum install openssl-devel -y
 cd ~/tool
 if [ -d "/root/tool/python-3.7.3/" ];then
 echo "文件夹存在"
 else
 echo "文件夹不存在"
-wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz                                                                                
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
 tar -xvJf  Python-3.7.3.tar.xz
 fi
 mkdir /usr/local/python3 #创建编译安装目录
 cd Python-3.7.3
-./configure --prefix=/usr/local/python3
+./configure --prefix=/usr/local/python3 --enable-optimizations
 # make && make install
 make
 make install
- 
+
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3
