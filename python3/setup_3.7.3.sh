007cd8f6ac50f69cf8da0f5e8a8e12c09bf8706a
chaosbaby <50092526@qq.com>
2020-02-04 00:32:40 +0800

    update python to 3.8.1

--- python3/setup_3.7.3.sh
+++ python3/setup_3.7.3.sh
@@ -1,3 +1,4 @@
+
 yum install zlib zlib-devel -y
 yum install libffi-devel -y
 yum install unzip wget -y
@@ -5,21 +6,21 @@ yum install ctags cmake gcc-c++ python-devel -y
 yum install openssl-devel -y
 yum -y install sqlite-devel -y
 cd ~/tool
-if [ -d "/root/tool/Python-3.7.3/" ];then
+if [ -d "/root/tool/Python-3.8.1/" ];then
 echo "文件夹存在"
 else
 echo "文件夹不存在"
-wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
-tar -xvJf  Python-3.7.3.tar.xz
+# wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+wget https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tar.xz
+tar -xvJf  Python-3.8.1.tar.xz
 fi
 mkdir /usr/local/python3 #创建编译安装目录
-cd Python-3.7.3
+cd Python-3.8.1
 ./configure --prefix=/usr/local/python3 --with-ssl
 # ./configure --prefix=/usr/local/python3 --enable-optimizations
 # make && make install
 make
 make install
-
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3
-ln -s /usr/local/python3/bin/python3.7m-config /usr/bin/python3.7m-config
+ln -s /usr/local/python3/bin/python3.8m-config /usr/bin/python3.8m-config
