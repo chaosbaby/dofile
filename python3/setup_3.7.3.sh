1ec1f1ddbfdb9def293d62c4af01ebca2e2a02e9
chaosbaby <50092526@qq.com>
2020-02-04 01:27:17 +0800

    update system time before install python3

--- python3/setup_3.7.3.sh
+++ python3/setup_3.7.3.sh
@@ -4,7 +4,9 @@ yum install libffi-devel -y
 yum install unzip wget -y
 yum install ctags cmake gcc-c++ python-devel -y
 yum install openssl-devel -y
-yum -y install sqlite-devel -y
+yum install sqlite-devel -y
+yum install ntpdate -y
+ntpdate -u cn.pool.ntp.org
 cd ~/tool
 if [ -d "/root/tool/Python-3.8.1/" ];then
 echo "文件夹存在"
@@ -16,8 +18,8 @@ tar -xvJf  Python-3.8.1.tar.xz
 fi
 mkdir /usr/local/python3 #创建编译安装目录
 cd Python-3.8.1
-./configure --prefix=/usr/local/python3 --with-ssl
-# ./configure --prefix=/usr/local/python3 --enable-optimizations
+# ./configure --prefix=/usr/local/python3 --with-ssl
+./configure --prefix=/usr/local/python3 --with-ssl --enable-optimizations
 # make && make install
 make
 make install
