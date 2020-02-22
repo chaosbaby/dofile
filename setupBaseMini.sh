1815cba11adc1db4e9ab9c85af9d1607c4232663
chaos <50092526@qq.com>
2020-02-23 03:00:06 +0800

    add base tool most useful and less time taken

---
+++ setupBaseMini.sh
@@ -0,0 +1,30 @@
+# base setting
+
+dofilesPath=`pwd`
+# for some more recent plugin
+sudo yum upgrade -y
+sudo yum update -y
+
+yum install net-tool -y #for wget
+
+#git setting
+yum install git -y
+git config --global user.email "50092526@qq.com"
+git config --global user.name "chaos"
+git config --global credential.helper store
+
+# hub install
+cd ${dofilesPath}/hub
+sh ${dofilesPath}/hub/setup.sh
+
+# python3
+cd ${dofilesPath}/python3
+sh ${dofilesPath}/python3/setup_3.7.3.sh
+
+## dofiles: tmux
+cd ${dofilesPath}/tmux
+sh ${dofilesPath}/tmux/setup.sh
+
+## dofiles: zsh
+cd ${dofilesPath}/zsh
+sh ${dofilesPath}/zsh/setup.sh
