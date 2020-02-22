225c83ac5e35eb0585850ec4572723ff2364b003
chaos <50092526@qq.com>
2020-02-23 03:35:13 +0800

    add nvim install and set nvim trans from vim setting

--- vim/nvim_noInstall.sh
+++ vim/nvim_noInstall.sh
@@ -1,7 +1,11 @@
 cd ~/tool
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
-ln -s nvim.appimage /usr/bin/nvim
 rm /usr/bin/nvim -f
 ln -s ~/tool/nvim.appimage /usr/bin/nvim
-nvim
+mkdir -p ~/.comfig/nvim/
+rm -f ~/.vimrc
+ln -s ~/tool/dofile/vim/init.vimrc ~/.vimrc
+echo 'set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc' >> ~/.comfig/nvim/init.vim
