0e595853796ee5c53a428d2a7161d15a1f6b872e
chaos <chaos@localhost.localdomain>
2020-03-05 19:01:02 -0500

    fzf change key <c-b> buffers

--- commandTools/yarn_centos8.sh
+++ commandTools/yarn_centos8.sh
@@ -3,4 +3,3 @@ sudo dnf install @nodejs
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
 sudo dnf install yarn
-
