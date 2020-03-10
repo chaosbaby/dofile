0edadbb0035065481673ddd778a8d9e68aa7f717
chaos <50092526@qq.com>
2020-03-10 09:32:23 -0400

    delete link cos having .cfg dotfiles instead

--- git/setup.sh
+++ git/setup.sh
@@ -5,8 +5,8 @@ git config --global user.email "50092526@qq.com"
 git config --global user.name "chaos"
 git config --global credential.helper store
 
-ln $curPath/.gitignore_global $HOME/.gitignore_global
-ln $curPath/.gitaliases $HOME/.gitaliases
+# ln $curPath/.gitignore_global $HOME/.gitignore_global
+# ln $curPath/.gitaliases $HOME/.gitaliases
 
 git config --global core.excludesfile $HOME/.gitignore_global
 git config --global include.path $HOME/.gitaliases
