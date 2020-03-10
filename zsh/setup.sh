d12cab661eacda92e01e7fc936b4bc01a9646b98
chaos <50092526@qq.com>
2020-03-10 09:57:25 -0400

    install zplug

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -4,4 +4,5 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
     echo "Install oh-my-zsh ..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 source ~/.zshrc
