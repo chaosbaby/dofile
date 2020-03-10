00a8d91306fc6ace6593d4907781c5332346afbc
chaos <50092526@qq.com>
2020-03-10 13:28:12 -0400

    delete link in nvim install

--- vim/nvim_noInstall.sh
+++ vim/nvim_noInstall.sh
@@ -1,16 +1,5 @@
 cd ~/tool
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-sudo chmod u+x nvim.appimage
+sudo chmod 777 nvim.appimage
 sudo rm /usr/bin/nvim -f
 sudo ln -s ~/tool/nvim.appimage /usr/bin/nvim
-sudo mkdir -p ~/.config/nvim/
-sudo rm -f ~/.vimrc
-
-echo 'set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-sudo ln -s ~/tool/dofile/vim/init.vimrc ~/.vimrc
-source ~/.vimrc' >> ~/.config/nvim/init.vim
-
-curFilePath=$(dirname $(readlink -f "$0"))
-echo $curFilePath
-ln -s $curFilePath/setting/plug $HOME/.config
