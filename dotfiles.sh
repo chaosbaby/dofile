d7d97f5c79ddf917dd79e92558cb3363334a372d
chaos <50092526@qq.com>
2020-03-11 15:49:57 -0400

    fix err use function to call a long line cmd

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,11 +1,21 @@
 if [ -d "$HOME/.cfg" ];then
     echo "文件夹存在"
 else
-cd ~
-git clone --bare https://github.com/chaosbaby/dotfiles.git $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
-mkdir -p .config-backup && config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-config checkout
+	cd ~
+	git clone --bare https://github.com/chaosbaby/dotfiles.git $HOME/.cfg
+	function config {
+	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+	}
+	mkdir -p .config-backup
+	config checkout
+	if [ $? = 0 ]; then
+		echo "Checked out config.";
+	else
+		echo "Backing up pre-existing dot files.";
+		config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+	fi;
+	config config --local status.showUntrackedFiles no
+	config checkout
+	config config status.showUntrackedFiles no
 fi
 
