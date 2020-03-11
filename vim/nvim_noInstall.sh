fa92849a18133ed3bf6b3853f94168fae65c1e2c
chaos <50092526@qq.com>
2020-03-11 16:20:00 -0400

    add vim plug after install nvim

--- vim/nvim_noInstall.sh
+++ vim/nvim_noInstall.sh
@@ -1,6 +1,14 @@
-cd ~/tool
+ld ~/tool
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 sudo chmod 777 nvim.appimage
 sudo rm /usr/bin/nvim -f
 sudo ln -s ~/tool/nvim.appimage /usr/bin/nvim
 sudo ln -s ~/tool/nvim.appimage /usr/bin/nv
+# for vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# for nvim
+# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
