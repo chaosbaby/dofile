bc5bfc26e1ab46f23dd5a8f1678648e81f600477
chaos <50092526@qq.com>
2020-03-11 21:17:08 -0400

    add bat install sh

---
+++ commandTools/bat.sh
@@ -0,0 +1,1 @@
+wget https://github.com/sharkdp/bat/releases/download/v0.7.1/bat-v0.7.1-x86_64-unknown-linux-musl.tar.gz -O - |tar -xvzf - -C /usr/local/bin && mv /usr/local/bin/bat-v0.7.1-x86_64-unknown-linux-musl/bat /usr/local/bin/bat
