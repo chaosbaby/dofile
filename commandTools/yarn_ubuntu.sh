b415b3562ea4747655c59aabd4a878f0cbde76dd
chaos <50092526@qq.com>
2020-04-18 04:23:01 +0800

    add yarn installation for ubuntu

---
+++ commandTools/yarn_ubuntu.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+sudo npm i -g npm --registry https://registry.npm.taobao.org
+sudo npm i -g yarn --registry https://registry.npm.taobao.org
+yarn config set registry https://registry.npm.taobao.org -g
+yarn config set sass_binary_site http://cdn.npm.taobao.org/dist/node-sass -g
+export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
+npm config set registry https://registry.npm.taobao.org
