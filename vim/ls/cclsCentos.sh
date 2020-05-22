950ddf29ad84bc23c6c430005e0345c9d5b76917
chaos <50092526@qq.com>
2020-05-22 17:00:53 -0400

    add ccls installing sh script

---
+++ vim/ls/cclsCentos.sh
@@ -0,0 +1,10 @@
+yum copr enable ngompa/snapcore-el7
+yum -y install snapd
+yum copr enable ngompa/snapcore-el7
+yum install epel-release
+yum install yum-plugin-copr
+yum copr enable ngompa/snapcore-el7
+yum -y install snapd
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
+snap install ccls --classic
