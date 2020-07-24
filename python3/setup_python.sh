88c7e8a062b7e9e5d774004f984e0255b3cec604
chaos <50092526@qq.com>
2020-07-24 14:36:04 +0800

    fixup! add install python with ver

--- python3/setup_python.sh
+++ python3/setup_python.sh
@@ -1,10 +1,12 @@
+sudo apt-get install libffi-dev -y
 pythonVer=$1
 
 url="https://www.python.org/ftp/python/$pythonVer/Python-$pythonVer.tar.xz"
 echo $url
 
+
 cd ~/tool
-if [ -d "/root/tool/Python-$pythonVer/" ];then
+if [ -d "$HOME/tool/Python-$pythonVer/" ];then
 echo "文件夹存在"
 else
 echo "文件夹不存在"
@@ -16,6 +18,4 @@ cd Python-$pythonVer
 # ./configure --prefix=/usr/local/python3 --with-ssl
 # ./configure --prefix=/usr/local/python-$pythonVer --with-ssl
 ./configure --prefix=/usr/local/python3 --enable-optimizations
-# make && make install
-sudo make
-sudo make install
+sudo make && make install
