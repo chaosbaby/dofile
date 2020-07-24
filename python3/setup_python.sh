7a03cdd345fdc049fe702fee8e74b84cf9d21ce1
chaos <50092526@qq.com>
2020-07-24 14:07:27 +0800

    add install python with ver

---
+++ python3/setup_python.sh
@@ -0,0 +1,21 @@
+pythonVer=$1
+
+url="https://www.python.org/ftp/python/$pythonVer/Python-$pythonVer.tar.xz"
+echo $url
+
+cd ~/tool
+if [ -d "/root/tool/Python-$pythonVer/" ];then
+echo "文件夹存在"
+else
+echo "文件夹不存在"
+wget $url
+tar -xvJf  Python-$pythonVer.tar.xz
+fi
+mkdir /usr/local/python-$pythonVer #创建编译安装目录
+cd Python-$pythonVer
+# ./configure --prefix=/usr/local/python3 --with-ssl
+# ./configure --prefix=/usr/local/python-$pythonVer --with-ssl
+./configure --prefix=/usr/local/python3 --enable-optimizations
+# make && make install
+sudo make
+sudo make install
