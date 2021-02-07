604d66bc9e4b6997e9e355aac02d98517377d153
chaos <50092526@qq.com>
2021-02-08 06:34:14 +0800

    add libsqlite3-dev before configure python3 for surport sqlite lib in python

--- python3/setup_python.sh
+++ python3/setup_python.sh
@@ -1,11 +1,13 @@
-sudo apt-get install libffi-dev -y
 pythonVer=$1
+sudo apt-get install libffi-dev -y
+sudo apt-get install sqlite3 -y
+sudo apt-get install libsqlite3-dev -y
 
 url="https://www.python.org/ftp/python/$pythonVer/Python-$pythonVer.tar.xz"
 echo $url
 
-
 cd ~/tool
+
 if [ -d "$HOME/tool/Python-$pythonVer/" ];then
 echo "文件夹存在"
 else
@@ -13,9 +15,11 @@ echo "文件夹不存在"
 wget $url
 tar -xvJf  Python-$pythonVer.tar.xz
 fi
+
 mkdir /usr/local/python-$pythonVer #创建编译安装目录
 cd Python-$pythonVer
 # ./configure --prefix=/usr/local/python3 --with-ssl
 # ./configure --prefix=/usr/local/python-$pythonVer --with-ssl
-./configure --prefix=/usr/local/python3 --enable-optimizations
-sudo make && make install
+sudo ./configure --prefix=/usr/local/python$pythonVer --enable-optimizations
+sudo make && sudo make install
+PATH=/usr/local/python$pythonVer/bin:$PATH
